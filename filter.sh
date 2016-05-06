b948c1f045ad31ea9c7e4da496d1ebe133fa805f
cympfh <cympfh@gmail.com>
2016-05-06 21:42:29 +0900

    fuck the mood

--- filter.sh
+++ filter.sh
@@ -2,4 +2,4 @@
 
 Q="#precure"
 twurl -t -d track=$Q -A 'Accept-Encoding: text' -H userstream.twitter.com /1.1/statuses/filter.json 2>/dev/null |
-jq --unbuffered -r '(if has("text") then "T " + .text else "" end) + "\n" + (if has ("entities") then (.entities.media | map(.media_url) | map("M " + .) | join("\n") ) else "" end)'
+jq --unbuffered -r '(if has("text") then "T " + (.text|gsub("\n";" ")) else "" end) + "\n" + (if has("entities") then (.entities | if has("media") then .media | map("M " + .media_url) | join("\n") else "" end ) else "" end)'
